15b4feeaff615d106c236f87bb2cf900574c3c15
Bruno Polaco Penteado <bpenteado@ig.com>
2012-04-05 15:08:23 -0300

    simplified and focused the var variables formula test

--- src/test/java/me/bcap/number/VariablesFormulaTest.java
+++ src/test/java/me/bcap/number/VariablesFormulaTest.java
@@ -5,83 +5,94 @@ import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
 
+import me.bcap.number.intf.VarDef;
+
 import org.junit.Test;
 
 public class VariablesFormulaTest {
 	
 	@Test
-	public void testAdditionWithVarsAndConstants() {
-		Formula formula = new Formula("x").plus(10);
-		assertThat(formula.calculate(new Var("x", 5.5)), is(new BigDecimal("15.5")));
-	}
-
-	@Test
-	public void testSubstractionWithVarsAndConstants() {
-		Formula formula = new Formula("x").minus(15.5);
-		assertThat(formula.calculate(new Var("x", 10)), is(new BigDecimal("-5.5")));
-	}
-
-	@Test
-	public void testMultiplicationWithVarsAndConstants() {
-		Formula formula = new Formula("x").times(10.1);
-		assertThat(formula.calculate(new Var("x", 5)), is(new BigDecimal("50.5")));
-	}
-
-	@Test
-	public void testDivisionWithVarsAndConstants() {
-		Formula formula = new Formula("x").dividedBy(2);
-		assertThat(formula.calculate(new Var("x", 5)), is(new BigDecimal("2.5")));
-	}
-	
-	@Test
-	public void testAdditionWithVarsOnly() {
-		Formula formula = new Formula("x").plus("y");
-		Var[] vars = new Var[] { new Var("x", 5.5), new Var("y", 10) };
-		assertThat(formula.calculate(vars), is(new BigDecimal("15.5")));
-	}
-
-	@Test
-	public void testSubstractionWithVarsOnly() {
-		Formula formula = new Formula("x").minus("y");
-		Var[] vars = new Var[] { new Var("x", 10), new Var("y", 15.5) };
-		assertThat(formula.calculate(vars), is(new BigDecimal("-5.5")));
-	}
-
-	@Test
-	public void testMultiplicationWithVarsOnly() {
-		Formula formula = new Formula("x").times("y");
-		Var[] vars = new Var[] { new Var("x", 5), new Var("y", 10.1) };
-		assertThat(formula.calculate(vars), is(new BigDecimal("50.5")));
-	}
-
-	@Test
-	public void testDivisionWithVarsOnly() {
-		Formula formula = new Formula("x").dividedBy("y");
-		Var[] vars = new Var[] { new Var("x", 5), new Var("y", 2) };
-		assertThat(formula.calculate(vars), is(new BigDecimal("2.5")));
-	}
-
-	@Test
-	public void testChainedCalculation() {
-		Formula formula = ((new Formula("a").plus("b")).times(new Formula("c").minus("d"))).dividedBy("e");
-
-		Var[] vars = new Var[] { 
-			new Var("a", 5),
-			new Var("b", 2),
-			new Var("c", 12),
-			new Var("d", 2),
-			new Var("e", 10)
+	public void testWithVarsAndConstants() {
+		Formula formula = (new Formula("x").plus(10).minus("y")).times(5).dividedBy("z");
+		VarDef[] vars = new VarDef[]{
+			new Var("x", 8),
+			new Var("y", 3),
+			new Var("z", 10)
 		};
-		assertThat(formula.calculate(vars), is(new BigDecimal("7")));
+		assertThat(formula.calculate(vars), is(new BigDecimal("7.5")));
+	}
+
+	@Test
+	public void testWithVarsOnly() {
+		Formula formula = (new Formula("x").plus("a").minus("y")).times("b").dividedBy("z");
+		VarDef[] vars = new VarDef[]{
+			new Var("x", 8),
+			new Var("y", 3),
+			new Var("z", 10),
+			new Var("a", 10),
+			new Var("b", 5)
+		};
+		assertThat(formula.calculate(vars), is(new BigDecimal("7.5")));
+	}
+
+	@Test
+	public void testValuesExercise() {
+		Formula formula = (new Formula("a").plus("b")).times("c");
+
+		class Expectation {
+			VarDef[] vars;
+			String result;
+		}
 		
-		vars = new Var[] { 
-			new Var("a", 1), 
-			new Var("b", 2),
-			new Var("c", 15),
-			new Var("d", 12),
-			new Var("e", 3)
+		Expectation[] expectations = new Expectation[] {
+			new Expectation() {{
+				vars = new VarDef[]{
+					new Var("a", 1),
+					new Var("b", 2),
+					new Var("c", 3)
+				};
+				result = "9";
+			}},
+			
+			new Expectation() {{
+				vars = new VarDef[]{
+					new Var("a", 2),
+					new Var("b", 3),
+					new Var("c", 4)
+				};
+				result = "20";
+			}},
+			
+			new Expectation() {{
+				vars = new VarDef[]{
+					new Var("a", 3),
+					new Var("b", 4),
+					new Var("c", 5)
+				};
+				result = "35";
+			}},
+			
+			new Expectation() {{
+				vars = new VarDef[]{
+					new Var("a", -1),
+					new Var("b", -2),
+					new Var("c", -1)
+				};
+				result = "3";
+			}},
+			
+			new Expectation() {{
+				vars = new VarDef[]{
+					new Var("a", 100),
+					new Var("b", -100),
+					new Var("c", 5)
+				};
+				result = "0";
+			}},
 		};
-		assertThat(formula.calculate(vars), is(new BigDecimal("3")));
+		
+		for (Expectation expectation : expectations) 
+			assertThat(formula.calculate(expectation.vars), is(equalTo(new BigDecimal(expectation.result))));
 	}
 	
 	@Test
