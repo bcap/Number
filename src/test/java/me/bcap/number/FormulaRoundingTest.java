4d31e69bdc51d8cc4b5966fa48fb5548d5169020
Bruno "Polaco" Penteado <polaco@gmail.com>
2012-04-06 15:00:59 -0300

    added more rouding result tests

--- src/test/java/me/bcap/number/FormulaRoundingTest.java
+++ src/test/java/me/bcap/number/FormulaRoundingTest.java
@@ -23,11 +23,120 @@ public class FormulaRoundingTest {
 	@Test
 	public void testResultRoundingHalfEven() {
 		RoundingMode rounding = RoundingMode.HALF_EVEN;
+		//positives
 		assertDivisionRounding("5.00", rounding, new Var("x", 10), new Var("y", 2));
 		assertDivisionRounding("3.33", rounding, new Var("x", 10), new Var("y", 3));
 		assertDivisionRounding("6.67", rounding, new Var("x", 20), new Var("y", 3));
-		assertDivisionRounding("0.00", rounding, new Var("x", 0.005), new Var("y", 1));
-		assertDivisionRounding("0.02", rounding, new Var("x", 0.015), new Var("y", 1));
+		assertDivisionRounding("0.00", rounding, new Var("x", 0.01), new Var("y", 2));
+		assertDivisionRounding("0.02", rounding, new Var("x", 0.03), new Var("y", 2));
+		//negatives
+		assertDivisionRounding("-5.00", rounding, new Var("x", -10), new Var("y", 2));
+		assertDivisionRounding("-3.33", rounding, new Var("x", -10), new Var("y", 3));
+		assertDivisionRounding("-6.67", rounding, new Var("x", -20), new Var("y", 3));
+		assertDivisionRounding("0.00",  rounding, new Var("x", -0.01), new Var("y", 2));
+		assertDivisionRounding("-0.02", rounding, new Var("x", -0.03), new Var("y", 2));
+	}
+	
+	@Test
+	public void testResultRoundingHalfDown() {
+		RoundingMode rounding = RoundingMode.HALF_DOWN;
+		//positives
+		assertDivisionRounding("5.00", rounding, new Var("x", 10), new Var("y", 2));
+		assertDivisionRounding("3.33", rounding, new Var("x", 10), new Var("y", 3));
+		assertDivisionRounding("6.67", rounding, new Var("x", 20), new Var("y", 3));
+		assertDivisionRounding("0.00", rounding, new Var("x", 0.01), new Var("y", 2));
+		assertDivisionRounding("0.01", rounding, new Var("x", 0.03), new Var("y", 2));
+		//negatives
+		assertDivisionRounding("-5.00", rounding, new Var("x", -10), new Var("y", 2));
+		assertDivisionRounding("-3.33", rounding, new Var("x", -10), new Var("y", 3));
+		assertDivisionRounding("-6.67", rounding, new Var("x", -20), new Var("y", 3));
+		assertDivisionRounding("0.00",  rounding, new Var("x", -0.01), new Var("y", 2));
+		assertDivisionRounding("-0.01", rounding, new Var("x", -0.03), new Var("y", 2));
+	}
+	
+	@Test
+	public void testResultRoundingHalfUp() {
+		RoundingMode rounding = RoundingMode.HALF_UP;
+		//positives
+		assertDivisionRounding("5.00", rounding, new Var("x", 10), new Var("y", 2));
+		assertDivisionRounding("3.33", rounding, new Var("x", 10), new Var("y", 3));
+		assertDivisionRounding("6.67", rounding, new Var("x", 20), new Var("y", 3));
+		assertDivisionRounding("0.01", rounding, new Var("x", 0.01), new Var("y", 2));
+		assertDivisionRounding("0.02", rounding, new Var("x", 0.03), new Var("y", 2));
+		//negatives
+		assertDivisionRounding("-5.00", rounding, new Var("x", -10), new Var("y", 2));
+		assertDivisionRounding("-3.33", rounding, new Var("x", -10), new Var("y", 3));
+		assertDivisionRounding("-6.67", rounding, new Var("x", -20), new Var("y", 3));
+		assertDivisionRounding("-0.01", rounding, new Var("x", -0.01), new Var("y", 2));
+		assertDivisionRounding("-0.02", rounding, new Var("x", -0.03), new Var("y", 2));
+	}
+	
+	@Test
+	public void testResultRoundingDown() {
+		RoundingMode rounding = RoundingMode.DOWN;
+		//positives
+		assertDivisionRounding("5.00", rounding, new Var("x", 10), new Var("y", 2));
+		assertDivisionRounding("3.33", rounding, new Var("x", 10), new Var("y", 3));
+		assertDivisionRounding("6.66", rounding, new Var("x", 20), new Var("y", 3));
+		assertDivisionRounding("0.00", rounding, new Var("x", 0.01), new Var("y", 2));
+		assertDivisionRounding("0.01", rounding, new Var("x", 0.03), new Var("y", 2));
+		//negatives
+		assertDivisionRounding("-5.00", rounding, new Var("x", -10), new Var("y", 2));
+		assertDivisionRounding("-3.33", rounding, new Var("x", -10), new Var("y", 3));
+		assertDivisionRounding("-6.66", rounding, new Var("x", -20), new Var("y", 3));
+		assertDivisionRounding("0.00",  rounding, new Var("x", -0.01), new Var("y", 2));
+		assertDivisionRounding("-0.01", rounding, new Var("x", -0.03), new Var("y", 2));
+	}
+	
+	@Test
+	public void testResultRoundingUp() {
+		RoundingMode rounding = RoundingMode.UP;
+		//positives
+		assertDivisionRounding("5.00", rounding, new Var("x", 10), new Var("y", 2));
+		assertDivisionRounding("3.34", rounding, new Var("x", 10), new Var("y", 3));
+		assertDivisionRounding("6.67", rounding, new Var("x", 20), new Var("y", 3));
+		assertDivisionRounding("0.01", rounding, new Var("x", 0.01), new Var("y", 2));
+		assertDivisionRounding("0.02", rounding, new Var("x", 0.03), new Var("y", 2));
+		//negatives
+		assertDivisionRounding("-5.00", rounding, new Var("x", -10), new Var("y", 2));
+		assertDivisionRounding("-3.34", rounding, new Var("x", -10), new Var("y", 3));
+		assertDivisionRounding("-6.67", rounding, new Var("x", -20), new Var("y", 3));
+		assertDivisionRounding("-0.01", rounding, new Var("x", -0.01), new Var("y", 2));
+		assertDivisionRounding("-0.02", rounding, new Var("x", -0.03), new Var("y", 2));
+	}
+	
+	@Test
+	public void testResultRoundingFloor() {
+		RoundingMode rounding = RoundingMode.FLOOR;
+		//positives
+		assertDivisionRounding("5.00", rounding, new Var("x", 10), new Var("y", 2));
+		assertDivisionRounding("3.33", rounding, new Var("x", 10), new Var("y", 3));
+		assertDivisionRounding("6.66", rounding, new Var("x", 20), new Var("y", 3));
+		assertDivisionRounding("0.00", rounding, new Var("x", 0.01), new Var("y", 2));
+		assertDivisionRounding("0.01", rounding, new Var("x", 0.03), new Var("y", 2));
+		//negatives
+		assertDivisionRounding("-5.00", rounding, new Var("x", -10), new Var("y", 2));
+		assertDivisionRounding("-3.34", rounding, new Var("x", -10), new Var("y", 3));
+		assertDivisionRounding("-6.67", rounding, new Var("x", -20), new Var("y", 3));
+		assertDivisionRounding("-0.01", rounding, new Var("x", -0.01), new Var("y", 2));
+		assertDivisionRounding("-0.02", rounding, new Var("x", -0.03), new Var("y", 2));
+	}
+	
+	@Test
+	public void testResultRoundingCeiling() {
+		RoundingMode rounding = RoundingMode.CEILING;
+		//positives
+		assertDivisionRounding("5.00", rounding, new Var("x", 10), new Var("y", 2));
+		assertDivisionRounding("3.34", rounding, new Var("x", 10), new Var("y", 3));
+		assertDivisionRounding("6.67", rounding, new Var("x", 20), new Var("y", 3));
+		assertDivisionRounding("0.01", rounding, new Var("x", 0.01), new Var("y", 2));
+		assertDivisionRounding("0.02", rounding, new Var("x", 0.03), new Var("y", 2));
+		//negatives
+		assertDivisionRounding("-5.00", rounding, new Var("x", -10), new Var("y", 2));
+		assertDivisionRounding("-3.33", rounding, new Var("x", -10), new Var("y", 3));
+		assertDivisionRounding("-6.66", rounding, new Var("x", -20), new Var("y", 3));
+		assertDivisionRounding("0.00", rounding, new Var("x", -0.01), new Var("y", 2));
+		assertDivisionRounding("-0.01", rounding, new Var("x", -0.03), new Var("y", 2));
 	}
 	
 	private void assertDivisionRounding(String result, RoundingMode rounding, VarDef... vars) {
