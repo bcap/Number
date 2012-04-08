293d2d851c35db650cb950cd2cc3827208c328f2
Bruno "Polaco" Penteado <polaco@gmail.com>
2012-04-08 20:41:24 -0300

    organize imports

--- src/test/java/me/bcap/number/SimpleFormulaTest.java
+++ src/test/java/me/bcap/number/SimpleFormulaTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
