1cef2d62afe7e2bd552525b460bfa8a844521d85
Bruno Polaco Penteado <bpenteado@ig.com>
2012-04-09 19:53:54 -0300

    added pythagorean theorem formula as an example

--- src/test/java/me/bcap/number/ExampleFormulasTest.java
+++ src/test/java/me/bcap/number/ExampleFormulasTest.java
@@ -10,18 +10,24 @@ import org.junit.Test;
 public class ExampleFormulasTest {
 
 	@Test
-	public void calculateSquareRoot() {
-		Formula formula = new Formula("y").withScale(500).plus(new Formula("x").withScale(500).dividedBy("y")).dividedBy(2);
-		//calculating the sqrt of 9 by using 9/2 as sample
+	public void calculatePythagoreanTheorem() {
+		/* Classic pythagorean theorem where a right triangle formed by
+		 * 
+		 *   |\
+		 * b | \ x
+		 *   |  \ 
+		 *   |___\
+		 *     a
+		 *     
+		 * follows the following formula x^2 = a^2 + b^2
+		 */
+
+		Formula a2 = new Formula("a").poweredBy(2);
+		Formula b2 = new Formula("b").poweredBy(2);
+		Formula x = (new Formula(a2).plus(b2)).squareRoot();
 		
-		int rootOf = 2;
-		
-		BigDecimal result = new Formula(rootOf).dividedBy(2).calculate();
-		BigDecimal lastCalculation = BigDecimal.ZERO;
-		while(result.subtract(lastCalculation).abs().compareTo(new BigDecimal("0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001")) > 0) {
-			lastCalculation = result;
-			result = formula.calculate(new Var("x", rootOf), new Var("y", lastCalculation));
-			System.out.println(result);
-		}
+		assertThat(x.calculate(new Var("a", 3), new Var("b", 4)), is(new BigDecimal(5)));
+		assertThat(x.calculate(new Var("a", 5), new Var("b", 12)), is(new BigDecimal(13)));
+		assertThat(x.calculate(new Var("a", 7), new Var("b", 24)), is(new BigDecimal(25)));
 	}
 }
